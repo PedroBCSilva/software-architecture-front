fe24d8e207f0a2b083d25365aeac56df6698ab3d
Pedro Bohlmann <pedro.b.c.silva@gmail.com>
2020-06-28 21:53:39 -0300

    Creating form for login on LoginPage

--- src/pages/login/login-page.tsx
+++ src/pages/login/login-page.tsx
@@ -1,12 +1,46 @@
 import React, { Component } from 'react'
 import { PageWrapper } from '../../components/index'
+import { Card } from '../../common-style/index'
+import { PageContentContainer, CardContent, FormTitle, FormLabel, FormInput, LoginButton, LoginButtonWrapper } from './style'
 
-export default class LoginPage extends Component{
+export default class LoginPage extends Component {
+    constructor(props:any){
+        super(props)
+    }
 
-    render(): JSX.Element{
+    renderLoginForm() {
+        return (
+            <>
+                <FormTitle>Login</FormTitle>
+                <FormLabel htmlFor='email'>Email</FormLabel>
+                <FormInput id='email' type='text' />
+                <FormLabel htmlFor='password'>Password</FormLabel>
+                <FormInput id='password' type='password' />
+                <LoginButtonWrapper>
+                    <LoginButton>
+                        Entrar
+                    </LoginButton>
+                </LoginButtonWrapper>
+            </>
+        )
+    }
+
+    renderLoginFormCard(): JSX.Element {
+        return (
+            <Card>
+                <CardContent>
+                    {this.renderLoginForm()}
+                </CardContent>
+            </Card>
+        )
+    }
+
+    render(): JSX.Element {
         return (
             <PageWrapper>
-                Login page
+                <PageContentContainer>
+                    {this.renderLoginFormCard()}
+                </PageContentContainer>
             </PageWrapper>
         )
     }
---
+++ src/pages/login/style.ts
@@ -0,0 +1,50 @@
+import styled from 'styled-components'
+
+export const PageContentContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: #f8f8f8;
+    min-height: 100vh
+`;
+
+export const CardContent = styled.div`
+    background: white;
+    width: 450px;
+    padding: 5px 10px;
+`;
+
+export const FormTitle = styled.h5`
+    font-size: 30px;
+    margin: 0;
+`;
+
+export const FormLabel = styled.label`
+    display: block;
+    margin: 5px 0px;
+`;
+
+export const FormInput = styled.input`
+    display: block;
+    margin: 5px 0px;
+    width: 100%;
+    height: 30px;
+`;
+
+export const LoginButtonWrapper = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const LoginButton = styled.button`
+    background-color: #4CAF50;
+    border: none;
+    color: white;
+    padding: 8px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+`;
