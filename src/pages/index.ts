ffad82dd8b6ce00e62f03017aef224254ccc8f4d
Pedro Bohlmann <pedro.bohlmann@sap.com>
2020-06-29 21:00:13 -0300

    Creating RegisterPage

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,7 +1,9 @@
 import HomePage from './home/home'
 import LoginPage from './login/login-page'
+import RegisterPage from './register/register-page'
 
 export {
     HomePage,
     LoginPage,
+    RegisterPage,
 }
