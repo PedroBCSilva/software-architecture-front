e8c77f946a740c38866cef1e28e9895d9060797e
Pedro Bohlmann <pedro.bohlmann@sap.com>
2020-06-30 21:37:52 -0300

    Renaming loginUser to  loginUserAction

--- src/redux/user/user-action.ts
+++ src/redux/user/user-action.ts
@@ -1,7 +1,7 @@
 import { LOGIN_USER_ACTION, UserActionTypes } from './types'
 import { User } from '../../types/user'
 
-export function loginUser(user: User): UserActionTypes {
+export function loginUserAction(user: User): UserActionTypes {
     return {
         type: LOGIN_USER_ACTION,
         payload: user,
