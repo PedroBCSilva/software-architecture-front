57efe292d1f3901b94fca30fa936681faa3014f7
Pedro Bohlmann <pedro.bohlmann@sap.com>
2020-07-01 23:13:27 -0300

    Validating form on LoginForm component

--- src/ui/components/login-form/login-form.tsx
+++ src/ui/components/login-form/login-form.tsx
@@ -1,24 +1,109 @@
 import React, { Component } from 'react'
-import { FormTitle, FormLabel, FormInput, Link, } from '../../common-style/index'
+import { FormTitle, FormLabel, FormInput, Link, ErrorMessage } from '../../common-style/index'
 import { RoutePaths } from '../../../constants/routes'
 import { LoginButton, LoginButtonWrapper } from './style'
+import { UserLoginRequest } from '../../../types/user'
+import UserService from '../../../service/user/user-service'
+import { AxiosResponse } from 'axios'
 
-export default class LoginForm extends Component {
+interface LoginFormProps { }
+
+interface LoginFormState {
+    email: string
+    password: string
+    formError: string
+}
+
+export default class LoginForm extends Component<LoginFormProps, LoginFormState> {
+    constructor(props: LoginFormProps) {
+        super(props)
+        this.state = {
+            email: '',
+            password: '',
+            formError: ''
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+
+    handleChange(event: React.ChangeEvent<HTMLInputElement>, callback = () => { }) {
+        const target = event.currentTarget
+        const value: string = target.value
+        const name: string = target.name
+        this.setState({
+            [name]: value
+        } as Pick<LoginFormState, any>, callback)
+    }
+
+    validateForm() {
+        if (!this.state.email || !this.state.password) {
+            this.setState({
+                formError: 'Por favor preencha os campos para realizar o login'
+            })
+            return false
+        }
+        else {
+            this.setState({
+                formError: ''
+            })
+            return true
+        }
+    }
+
+    getUserLoginRequestFromForm(): UserLoginRequest {
+        return {
+            email: this.state.email,
+            password: this.state.password,
+        }
+    }
+
+    onSubmit() {
+        if (this.validateForm()) {
+            UserService.loginRequest(this.getUserLoginRequestFromForm()).then(this.onSuccessfulLogin)
+                .catch(this.onFailLogin)
+        }
+    }
+
+    onSuccessfulLogin(response: AxiosResponse) {
+        console.log(response)
+    }
+
+    onFailLogin(error: any) {
+        console.log(error)
+    }
+
+
     render(): JSX.Element {
         return (
-            <>
+            <form onSubmit={this.onSubmit}>
                 <FormTitle>Login</FormTitle>
                 <FormLabel htmlFor='email'>Email</FormLabel>
-                <FormInput id='email' type='text' />
+                <FormInput name='email'
+                    id='email'
+                    type='email'
+                    onChange={this.handleChange}
+                    value={this.state.email}
+                    required
+                />
                 <FormLabel htmlFor='password'>Password</FormLabel>
-                <FormInput id='password' type='password' />
-                <Link href={RoutePaths.REGISTER}>Não tem conta? Clique aqui para se cadastrar</Link>
+                <FormInput name='password'
+                    id='password'
+                    type='password'
+                    onChange={this.handleChange}
+                    value={this.state.password}
+                    required
+                />
+                <ErrorMessage>{this.state.formError}</ErrorMessage>
+                <Link href={RoutePaths.REGISTER}>
+                    Não tem conta? Clique aqui para se cadastrar
+                </Link>
                 <LoginButtonWrapper>
-                    <LoginButton>
+                    <LoginButton onClick={this.onSubmit} type="submit">
                         Entrar
                     </LoginButton>
                 </LoginButtonWrapper>
-            </>
+            </form>
         )
     }
 }
